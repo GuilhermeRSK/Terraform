30a0bb62897b9c0ca9882cecce294aec323ec872
GuilhermeRSK <guilhermekurowski@hotmail.com>
2019-09-03 19:35:14 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,8 @@
 provider "azurerm" {
-  subscription_id = "1408d3e7-c2b9-4f24-ab23-d544d831598d"
-  client_id = "7b2b6ca1-7fef-42f3-8dfa-8381e903a77c"
-  client_secret = "78ba5599-42d4-4086-b17d-294bda80b7b0"
-  tenant_id = "11dbbfe2-89b8-4549-be10-cec364e59551"
+  subscription_id = "xxxxxxxx-c2b9-4f24-ab23-d544d831598d"
+  client_id = "xxxxxxxx-7fef-42f3-8dfa-8381e903a77c"
+  client_secret = "xxxxxxxx-42d4-4086-b17d-294bda80b7b0"
+  tenant_id = "xxxxxxxx-89b8-4549-be10-cec364e59551"
 }
 
 variable "subscription_id" {
